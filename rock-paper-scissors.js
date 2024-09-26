83b1bf8dde92c82ec5a26aecd4f26cd3a3f18727
jefferyrosario <jeffrosario92@gmail.com>
2024-09-25 20:57:06 -0400

    update rock paper scissor game

---
+++ rock-paper-scissors.js
@@ -0,0 +1,91 @@
+// This game is rock paper scissors. 
+
+function getComputerChoice() {
+
+    // Make a selection for the computer that is rock, paper, or scissors
+    const randomInt = Math.round(Math.random() * (2 - 0) + 0);
+    let computerChoice;
+    switch (randomInt) {
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissor";
+            break;
+    }
+    return computerChoice;
+}
+
+// Take input from user that is rock, paper or scissors
+function getHumanChoice(){
+    let humanChoice = prompt("Choose one: rock, paper, scissors");
+    return humanChoice;
+}
+
+// Create a function that plays one round
+function playRound(computerChoice, humanChoice) {
+    
+    let score;
+    switch (true) {
+        case computerChoice == "scissor" && humanChoice == "scissor":
+            console.log("Tie!")
+            break;
+        case computerChoice == "scissor" && humanChoice == "paper":
+            computerScore += 1
+            console.log("You lose! Scissors beats paper!")
+            break;
+        case computerChoice == "scissor" && humanChoice == "rock":
+            humanChoice += 1
+            console.log("You win! Rock beats scissors!")
+            break;
+        case computerChoice == "paper" && humanChoice == "paper":
+            console.log("Tie!")
+            break;
+        case computerChoice == "paper" && humanChoice == "rock":
+            console.log("You win! Rock beats paper!")
+            computerScore += 1;
+            break;
+        case computerChoice == "paper" && humanChoice == "scissors":
+            humanScore += 1;
+            console.log("You win! Scissors beats paper!")
+            break;
+        case computerChoice == "rock" && humanChoice == "rock":
+            console.log("Tie!")
+            break;
+        case computerChoice == "rock" && humanChoice == "paper":
+            humanScore += 1;
+            console.log("You win! Paper beats rock!")
+            break;
+        case computerChoice == "rock" && humanChoice == "scissor":
+            computerScore += 1;
+            console.log("You lose! Rock beats scissors!")
+    }
+}
+
+// Create a function that plays the entire game
+
+function playGame(){
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+
+    }
+
+    console.log("Computer Score is:"+computerScore)
+    console.log("Your score is:"+humanScore)
+}
+
+console.log(getComputerChoice())
+
+// Declare a variable to keep score of user
+let humanScore = 0;
+// Declare a variable to keep score of computer
+let computerScore = 0;
+
+playGame()
+
+//playRound(getComputerChoice(), getHumanChoice())
