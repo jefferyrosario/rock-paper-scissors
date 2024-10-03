fe0b69d14f0399b79205b4474de16e886da95bca
jefferyrosario <jeffrosario92@gmail.com>
2024-10-02 22:31:54 -0400

    Update css javascript and html

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -13,7 +13,7 @@ function getComputerChoice() {
             computerChoice = "paper";
             break;
         case 2:
-            computerChoice = "scissor";
+            computerChoice = "scissors";
             break;
     }
     return computerChoice;
@@ -21,110 +21,114 @@ function getComputerChoice() {
 
 // Take input from user that is rock, paper or scissors
 function getHumanChoice() {
-    let humanChoice = prompt("Choose one: rock, paper, scissors");
+  //  let humanChoice = prompt("Choose one: rock, paper, scissors");
+
     return humanChoice;
 }
 
 // Create a function that plays one round
-function playRound(computerChoice, humanChoice) {
 
-    let score;
-    switch (true) {
-        case computerChoice == "scissor" && humanChoice == "scissor":
-            console.log("Tie!")
-            break;
-        case computerChoice == "scissor" && humanChoice == "paper":
-            computerScore += 1
-            console.log("You lose! Scissors beats paper!")
-            break;
-        case computerChoice == "scissor" && humanChoice == "rock":
-            humanChoice += 1
-            console.log("You win! Rock beats scissors!")
-            break;
-        case computerChoice == "paper" && humanChoice == "paper":
-            console.log("Tie!")
-            break;
-        case computerChoice == "paper" && humanChoice == "rock":
-            console.log("You win! Rock beats paper!")
-            computerScore += 1;
-            break;
-        case computerChoice == "paper" && humanChoice == "scissors":
-            humanScore += 1;
-            console.log("You win! Scissors beats paper!")
-            break;
-        case computerChoice == "rock" && humanChoice == "rock":
-            console.log("Tie!")
-            break;
-        case computerChoice == "rock" && humanChoice == "paper":
-            humanScore += 1;
-            console.log("You win! Paper beats rock!")
-            break;
-        case computerChoice == "rock" && humanChoice == "scissor":
-            computerScore += 1;
-            console.log("You lose! Rock beats scissors!")
-    }
-}
 
 // Add Event Handler that call the playRound function with the correct playerSelection
-const btn = document.querySelectorAll('#btn')
+const btn = document.querySelectorAll('.btn')
 const rock = document.querySelector('#rock')
 const paper = document.querySelector('#paper')
 const scissors = document.querySelector('#scissors')
+const roundResult = document.querySelector('.roundResult')
+
+
+const computerScore = document.querySelector('.computerScore')
+const humanScore = document.querySelector('.humanScore')
+const finalResult = document.querySelector('.finalResult')
+function incrementScore(score){
+    let actualScore = 0;
+    actualScore = score.textContent
+    actualScore += 1;
+    computerScore.textContent = actualScore
+  //  return actualScore;
+}
 
 btn.forEach((button) => {
+    let computerChoice = "";
+    let buttonSelected = "";
     button.addEventListener("click", () => {
-        playRound(getComputerChoice(),)
+        finalResult.textContent = ""
+        buttonSelected = button.textContent
+        playRound(getComputerChoice(), buttonSelected)
+        
+      //  computerScore.textContent = computerScore
+     //   humanScore.textContent = humanScore
+
+        if(parseInt(computerScore.textContent) == 5){
+            finalResult.textContent = "You lose the game! Game Over!"
+            resetGame()
+        } else if (parseInt(humanScore.textContent) == 5){
+            finalResult.textContent = "You win the game!"
+            resetGame()
+        }
     })
 })
 
-function playRound() {
+function resetGame(){
+    computerScore.textContent = 0;
+    humanScore.textContent = 0;
+
+}
+
+function playRound(computerChoice, humanChoice) {
     let score;
     switch (true) {
-        case computerChoice == "scissor" && humanChoice == "scissor":
-            console.log("Tie!")
+        case computerChoice == "scissors" && humanChoice == "scissors":
+            roundResult.textContent = "Tie!"
             break;
-        case computerChoice == "scissor" && humanChoice == "paper":
-            computerScore += 1
-            console.log("You lose! Scissors beats paper!")
+        case computerChoice == "scissors" && humanChoice == "paper":
+            computerScore.textContent = parseInt(computerScore.textContent) + 1
+           // incrementScore(computerScore)
+            roundResult.textContent = "You lose! Scissors beats paper!"
             break;
-        case computerChoice == "scissor" && humanChoice == "rock":
-            humanChoice += 1
-            console.log("You win! Rock beats scissors!")
+        case computerChoice == "scissors" && humanChoice == "rock":
+            humanChoice.textContent = parseInt(humanScore.textContent) + 1
+            roundResult.textContent = "You win! Rock beats scissors!"
             break;
         case computerChoice == "paper" && humanChoice == "paper":
-            console.log("Tie!")
+            roundResult.textContent = "Tie!" 
             break;
         case computerChoice == "paper" && humanChoice == "rock":
-            console.log("You win! Rock beats paper!")
-            computerScore += 1;
+            roundResult.textContent = "You win! Rock beats paper!"
+            computerScore.textContent = parseInt(computerScore.textContent) + 1;
             break;
         case computerChoice == "paper" && humanChoice == "scissors":
-            humanScore += 1;
-            console.log("You win! Scissors beats paper!")
+            humanScore.textContent = parseInt(humanScore.textContent) + 1;
+            roundResult.textContent = "You win! Scissors beats paper!"
             break;
         case computerChoice == "rock" && humanChoice == "rock":
-            console.log("Tie!")
+            roundResult.textContent = "Tie!"
             break;
         case computerChoice == "rock" && humanChoice == "paper":
-            humanScore += 1;
-            console.log("You win! Paper beats rock!")
+            humanScore.textContent = parseInt(humanScore.textContent) + 1;
+            roundResult.textContent = "You win! Paper beats rock!"
             break;
-        case computerChoice == "rock" && humanChoice == "scissor":
-            computerScore += 1;
-            console.log("You lose! Rock beats scissors!")
+        case computerChoice == "rock" && humanChoice == "scissors":
+            computerScore.textContent = parseInt(computerScore.textContent) + 1;
+            roundResult.textContent = "You lose! Rock beats scissors!" 
     }
 }
 
-// Create a function that plays the entire game
 
 
-
-console.log(getComputerChoice())
 
 // Declare a variable to keep score of user
-let humanScore = 0;
+//let humanScore = 0;
 // Declare a variable to keep score of computer
-let computerScore = 0;
+//let computerScore = 0;
+
+function playGame(computerChoice, humanChoice) {
+    if(humanScore == 5 || computerScore == 5){
+        
+        humanScore = 0
+        computerScore = 0
+    }
+}
 
 playGame()
 
